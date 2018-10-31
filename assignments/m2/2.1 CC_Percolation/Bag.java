409394e05addfb9ca95722e24f906e77e9c19a0f
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-31 10:27:09 +0530

    m2

--- assignments/m2/2.1 CC_Percolation/Bag.java
+++ assignments/m2/2.1 CC_Percolation/Bag.java
@@ -1,79 +1,112 @@
-import java.util.*;
+import java.util.Iterator;
+/**.
+ * Class for bag.
+ *
+ * @param      <Item>  The item
+ */
 public class Bag<Item> implements Iterable<Item> {
-    private Node<Item> first;    // beginning of bag
-    private int n;               // number of elements in bag
-
-    // helper linked list class
-    private static class Node<Item> {
+    /**.
+     * { var_description }
+     */
+    private int n;
+    /**.
+     * { var_description }
+     */
+    private Node first;
+    /**.
+     * Class for node.
+     */
+    private class Node {
+        /**.
+         * { var_description }
+         */
         private Item item;
-        private Node<Item> next;
+        /**.
+         * { var_description }
+         */
+        private Node next;
     }
 
-    /**
-     * Initializes an empty bag.
+   /**
+     * Create an empty stack.
      */
     public Bag() {
         first = null;
         n = 0;
     }
-
-    /**
-     * Returns true if this bag is empty.
-     *
-     * @return {@code true} if this bag is empty;
-     *         {@code false} otherwise
-     */
+/**.
+ * Determines if empty.
+ *
+ * @return     True if empty, False otherwise.
+ * time complexity is 1
+ */
     public boolean isEmpty() {
         return first == null;
     }
-
-    /**
-     * Returns the number of items in this bag.
-     *
-     * @return the number of items in this bag
-     */
+/**.
+ * { function_description }
+ *
+ * @return     { description_of_the_return_value }
+ * time complexity is 1
+ */
     public int size() {
         return n;
     }
-
-    /**
-     * Adds the item to this bag.
-     *
-     * @param  item the item to add to this bag
-     */
-    public void add(Item item) {
-        Node<Item> oldfirst = first;
-        first = new Node<Item>();
+/**.
+ * { function_description }
+ *
+ * @param      item  The item
+ * time complexity is 1
+ */
+    public void add(final Item item) {
+        Node oldfirst = first;
+        first = new Node();
         first.item = item;
         first.next = oldfirst;
         n++;
     }
-
-
-    /**
-     * Returns an iterator that iterates over the items in this bag in arbitrary order.
-     *
-     * @return an iterator that iterates over the items in this bag in arbitrary order
-     */
+/**.
+ * { function_description }
+ *
+ * @return     { description_of_the_return_value }
+ * time complexity is 1
+ */
     public Iterator<Item> iterator()  {
-        return new ListIterator<Item>(first);  
+        return new ListIterator();
     }
-
-    // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator<Item> implements Iterator<Item> {
-        private Node<Item> current;
-
-        public ListIterator(Node<Item> first) {
-            current = first;
+/**.
+ * Class for list iterator.
+ */
+    private class ListIterator implements Iterator<Item> {
+        /**.
+         * { var_description }
+         */
+        private Node current = first;
+        /**.
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         * time complexity is 1
+         */
+        public boolean hasNext() {
+            return current != null;
         }
-
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
+        /**.
+         * { function_description }
+         * time complexity is 1
+         */
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+        /**.
+         * { function_description }
+         *
+         * @return     { description_of_the_return_value }
+         * time complexity is 1
+         */
         public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
             Item item = current.item;
-            current = current.next; 
+            current = current.next;
             return item;
         }
     }
--- assignments/m2/2.1 CC_Percolation/Graph.java
+++ assignments/m2/2.1 CC_Percolation/Graph.java
@@ -1,80 +1,95 @@
-import java.util.*;
-
-public class Graph {
-    private static final String NEWLINE = System.getProperty("line.separator");
-
-    private final int V;
-    private int E;
+/**.
+ * List of graphs.
+ */
+class Graph {
+    /**.
+     * { var_description }.
+     */
+    private int ver;
+    /**.
+     * { var_description }
+     */
+    private int edg;
+    /**.
+     * { var_description }
+     */
     private Bag<Integer>[] adj;
-    
+    /**.
+     * { var_description }
+     */
+    private String[] vertexes;
+    /**.
+     * { var_description }
+     */
+    private int size = 0;
     /**
-     * Initializes an empty graph with {@code V} vertices and 0 edges.
+     * Initializes an empty graph with V vertices and 0 edges.
      * param V the number of vertices
      *
-     * @param  V number of vertices
-     * @throws IllegalArgumentException if {@code V < 0}
+     * @param  vt number of vertices
      */
-    public Graph(int V) {
-        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
-        this.V = V;
-        this.E = 0;
-        adj = (Bag<Integer>[]) new Bag[V];
-        for (int v = 0; v < V; v++) {
+    Graph(final int vt) {
+        this.ver = vt;
+        this.edg = 0;
+        vertexes = new String[ver];
+        size = 0;
+        adj = (Bag<Integer>[]) new Bag[ver];
+        for (int v = 0; v < ver; v++) {
             adj[v] = new Bag<Integer>();
         }
     }
 
-    
-
     /**
      * Returns the number of vertices in this graph.
-     *
+     * time complexity is 1
      * @return the number of vertices in this graph
      */
-    public int V() {
-        return V;
+    public int vert() {
+        return ver;
     }
 
     /**
      * Returns the number of edges in this graph.
-     *
+     * time complexity is 1
      * @return the number of edges in this graph
      */
-    public int E() {
-        return E;
+    public int edge() {
+        return edg;
     }
-
-    // throw an IllegalArgumentException unless {@code 0 <= v < V}
-    private void validateVertex(int v) {
-        if (v < 0 || v >= V)
-            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    /**.
+     * Adds a vertex.
+     * time complexity is 1
+     * @param      v     { parameter_description }
+     */
+    public void addVertex(final String v) {
+        vertexes[size] = v;
+        size++;
     }
-
     /**
      * Adds the undirected edge v-w to this graph.
-     *
+     * time complexity is 1
      * @param  v one vertex in the edge
      * @param  w the other vertex in the edge
-     * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
      */
-    public void addEdge(int v, int w) {
-        validateVertex(v);
-        validateVertex(w);
-        E++;
+    public void addEdge(final int v, final int w) {
+        if (v == w) {
+            System.out.println(ver + " vertices, " + edg + " edges");
+            System.out.println("No edges");
+            return;
+        }
+            edg++;
         adj[v].add(w);
         adj[w].add(v);
     }
-
-
     /**
      * Returns the vertices adjacent to vertex {@code v}.
      *
      * @param  v the vertex
      * @return the vertices adjacent to vertex {@code v}, as an iterable
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     * time complexity is 1
      */
-    public Iterable<Integer> adj(int v) {
-        validateVertex(v);
+    public Iterable<Integer> adj(final int v) {
         return adj[v];
     }
 
@@ -84,9 +99,9 @@ public class Graph {
      * @param  v the vertex
      * @return the degree of vertex {@code v}
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     * time complexity is 1
      */
-    public int degree(int v) {
-        validateVertex(v);
+    public int degree(final int v) {
         return adj[v].size();
     }
 
@@ -94,22 +109,22 @@ public class Graph {
     /**
      * Returns a string representation of this graph.
      *
-     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
+     * @return the number of vertices <em>V</em>, followed by
+     * the number of edges <em>E</em>,
      *         followed by the <em>V</em> adjacency lists
+     * time complexity is N^2
      */
     public String toString() {
-        StringBuilder s = new StringBuilder();
-        s.append(V + " vertices, " + E + " edges " + NEWLINE);
-        for (int v = 0; v < V; v++) {
-            s.append(v + ": ");
+        String s = "";
+        s += ver + " vertices, " + edg + " edges" + '\n';
+        for (int v = 0; v < ver; v++) {
+            s += vertexes[v] + ": ";
             for (int w : adj[v]) {
-                s.append(w + " ");
+                s += vertexes[w] + " ";
             }
-            s.append(NEWLINE);
+            s += '\n';
         }
-        return s.toString();
+        return s;
     }
 
-
-    
 }
