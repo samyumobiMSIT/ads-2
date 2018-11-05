327eb31ba8b6285a5457f0863985c7a73e09bc33
samyumobiMSIT <samyumobi@msitprogram.net>
2018-11-06 01:12:04 +0530

    "ads2_m7_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m7/1. MST/Solution.java
+++ assignments/m7/1. MST/Solution.java
@@ -2,32 +2,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
-
 public class Solution {
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         // TODO Auto-generated method stub
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); 
+        int T=Integer.parseInt(br.readLine());
         String s;
-        StringTokenizer st;
-        while (T > 0) {
-            if ((s = br.readLine().trim()) != null) {
-                st = new StringTokenizer(s);
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                System.out.println(a - 1);
-                while (b > 0) {
-                    if ((s = br.readLine().trim()) != null) {
-
+        StringTokenizer st;     
+        while(T>0)
+        {
+            if((s=br.readLine().trim())!=null)
+            {
+                st=new StringTokenizer(s);
+                int a=Integer.parseInt(st.nextToken());
+                int b=Integer.parseInt(st.nextToken());
+                System.out.println(a-1);
+                while(b>0)
+                {
+                    if((s=br.readLine().trim())!=null)
+                    {
+                    
                         b--;
                     }
                 }
-
+                
                 T--;
-
+                
+                    
             }
         }
 
