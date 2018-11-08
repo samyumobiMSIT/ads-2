d5af1e2b509b0fa76590798f7fcd4bac83f1c10d
samyumobiMSIT <samyumobi@msitprogram.net>
2018-11-08 13:41:59 +0530

    "ads2_m10_a1_1: 3/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practise/Seam Carving/SeamCarver.java
+++ practise/Seam Carving/SeamCarver.java
@@ -1,253 +1,216 @@
-import java.util.*;
-import java.lang.*;
 import java.awt.Color;
-//import edu.princeton.cs.algs4.*;
+
+/*import edu.princeton.cs.algs4.IndexMinPQ;
+import edu.princeton.cs.algs4.Picture;*/
 
 public class SeamCarver {
+    static final private double BORDERENERGY=1000;
+    private Picture picture;
+    private Color[][] colorMatrix;
+    private double[] distTo;
+    private int[] edgeTo;
+    
+    //Warning: this is the opposite of the standard mathematical notation used in linear algebra where (i, j) refers to row i and column j and with Cartesian coordinates where (0, 0) is at the lower left corner.
+    public SeamCarver(Picture picture) {                // create a seam carver object based on the given picture
+        if (picture==null)
+            throw new java.lang.IllegalArgumentException();
+        this.picture=picture;
+        colorMatrix=new Color[picture.height()][picture.width()];
+        for(int i=0;i<picture.height();i++)
+            for(int j=0;j<picture.width();j++)
+                colorMatrix[i][j]=picture.get(j, i);
+        distTo=new double[width()*height()];
+        edgeTo=new int[width()*height()];
+    }
+    
 
-    private double[][] energies;
-    private Color[][] colors;
-    private int width;
-    private int height;
-    private enum ORIENTATION { HORIZONTAL, VERTICAL };
-    private ORIENTATION orientation;
 
 
-    public SeamCarver(Picture picture){
-        if (picture == null)
-            throw new java.lang.IllegalArgumentException("Null picture");
-
-        width = picture.width();
-        height = picture.height();
-        colors = new Color[height][width];
-        orientation = ORIENTATION.VERTICAL;
-
-
-        for (int row=0; row < height; row++) {
-            for (int col=0; col < width; col++) {
-                colors[row][col] = picture.get(col, row);
+    public Picture picture() {                          // current picture
+        int height=colorMatrix.length;
+        int width=colorMatrix[0].length;
+        System.out.println(height+"  "+width);
+        Picture newPicture = new Picture(width,height);
+        for (int x = 0; x < height; x++) {
+            for (int y = 0; y < width; y++) {
+                newPicture.set(y, x, colorMatrix[x][y]);
             }
         }
-
-        fillEnergies();
+        this.picture=newPicture;
+        return newPicture;
     }
-
-    public Picture picture() {
-        transposeV();
-
-        Picture picture = new Picture(width, height);
-        for (int row=0; row < height; row++) {
-            for (int col=0; col < width; col++) {
-                picture.set(col, row, colors[row][col]);
+    
+    public int width() {                            // width of current picture
+        return picture.width();
+    }
+    
+    public int height() {                           // height of current picture
+        return picture.height();
+    }
+    public double energy(int x, int y) {               // energy of pixel at column x and row y
+        if(x<0||x>=width()||y<0||y>=height())
+            throw new java.lang.IllegalArgumentException("");
+        if(x==0||x==colorMatrix[0].length-1||y==0||y==colorMatrix.length-1)
+            return BORDERENERGY;
+        final Color leftPixel=colorMatrix[y][x-1];
+        final Color rightPixel=colorMatrix[y][x+1];
+        final Color upPixel=colorMatrix[y-1][x];
+        final Color downPixel=colorMatrix[y+1][x];
+        double redX=leftPixel.getRed()-rightPixel.getRed();
+        double greenX=leftPixel.getGreen()-rightPixel.getGreen();
+        double blueX=leftPixel.getBlue()-rightPixel.getBlue();
+        double redY=upPixel.getRed()-downPixel.getRed();
+        double greenY=upPixel.getGreen()-downPixel.getGreen();
+        double blueY=upPixel.getBlue()-downPixel.getBlue();
+        return Math.sqrt(redX*redX+greenX*greenX+blueX*blueX+redY*redY+greenY*greenY+blueY*blueY);
+    }
+    
+    private void DijkstraSP(boolean isVertical) {
+        int V=width()*height();
+        for(int v=0;v<V;v++) {
+            distTo[v]=Double.POSITIVE_INFINITY;
+            edgeTo[v]=0;
+        }
+        int Len=width();
+        if(!isVertical)
+            Len=height();
+        IndexMinPQ<Double>pq=new IndexMinPQ<>(V);
+        //the all pixels at top row are source
+        for(int v=0;v<Len;v++) {
+            distTo[v]=BORDERENERGY;
+            pq.insert(v, distTo[v]);
+        }
+        boolean notFinStat=true;
+        while(!pq.isEmpty()&&notFinStat) {
+            int v=pq.delMin();
+            relax(v,isVertical,pq);
+            //ensure there is at least a path to bottom rather than left or right
+            for(int i=V-1;i>=V-Len;--i) {
+                if(distTo[i]!=Double.POSITIVE_INFINITY) {
+                    notFinStat=false;
+                    break;
+                }
             }
         }
-
-        return picture;
+        
     }
-
-    public int width() {
-        return orientation == ORIENTATION.VERTICAL ? width : height;
-    }
-
-
-    public int height()  {
-        return orientation == ORIENTATION.VERTICAL ? height : width;
-    }
-
-
-    private double gradient(int col, int row) {
-        Color left = colors[row][col - 1];
-        Color right = colors[row][col + 1];
-        Color top = colors[row - 1][col];
-        Color bottom = colors[row + 1][col];
-
-        return Math.pow((centralDifferences(left, right) +
-                         centralDifferences(top, bottom)), 0.5);
-    }
-
-    private double centralDifferences(Color c1, Color c2) {
-        return Math.pow(c1.getRed() - c2.getRed(), 2) +
-               Math.pow(c1.getBlue() - c2.getBlue(), 2) +
-               Math.pow(c1.getGreen() - c2.getGreen(), 2);
-    }
-
-    public int[] findHorizontalSeam() {
-        transposeH();
-
-        int[] seam = findVerticalSeam();
-
-        orientation = ORIENTATION.HORIZONTAL;
-        return seam;
-    }
-
-    public int[] findVerticalSeam()  {
-        Point2D edgeTo[][];
-        double distTo[][];
-
-        transposeV();
-
-        edgeTo = new Point2D[height][width];
-        distTo = new double[height][width];
-
-        initDistToAndEdgeTo(edgeTo, distTo);
-        for (int row=0; row < height; row++){
-            for (int col=0; col < width; col++) {
-                relaxVertex(row, col, edgeTo, distTo);
-            }
+    
+    private void relax(int v, boolean isVertical,IndexMinPQ<Double> pq) {
+        // TODO Auto-generated method stub
+        int x,y,w;
+        double weight;
+        int width=width();
+        int height=height();
+        if(!isVertical) {
+            width=height();
+            height=width();
         }
-
-
-        return trace(edgeTo, distTo);
-    }
-
-    private int[] trace(Point2D edgeTo[][], double[][] distTo) {
-        int curCol = -1;
-        double minDist = Double.MAX_VALUE;
-        int[] seam = new int[height];
-
-        for (int col=0; col < width; col++) {
-            if (distTo[height - 1][col] < minDist) {
-                curCol = col;
-                minDist = distTo[height - 1][curCol];
-            }
-        }
-
-        for (int row= height - 1; row > 0; row--){
-            seam[row] = curCol;
-            curCol = (int) edgeTo[row][curCol].y();
-        }
-        seam[0] = curCol;
-
-        return seam;
-    }
-
-
-    private void initDistToAndEdgeTo(Point2D[][] edgeTo, double[][] distTo) {
-        for (int row=0; row < height; row++) {
-            for (int col=0; col < width; col++) {
-                distTo[row][col] = Double.MAX_VALUE;
-                edgeTo[row][col] = null;
-            }
-        }
-        for (int col=0; col < width; col++)
-            distTo[0][col] = 0;
-    }
-
-    private ArrayList<Point2D> reachableVertices(int row, int col) {
-        ArrayList<Point2D> reachable = new ArrayList<Point2D>();
-
-        if (row == height - 1) return reachable;
-
-        if (col > 0) reachable.add(new Point2D (row + 1, col - 1));
-        if (col < width - 1) reachable.add(new Point2D(row + 1, col + 1));
-        reachable.add(new Point2D(row + 1, col));
-
-        return reachable;
-    }
-
-    private void relaxVertex(int row, int col, Point2D[][] edgeTo, double[][] distTo) {
-
-        ArrayList<Point2D> reachable = reachableVertices(row, col);
-        for (Point2D sink : reachable)
-            relaxEdge((int) sink.x(), (int) sink.y(), row, col, edgeTo, distTo);
-    }
-
-    private void relaxEdge(int row, int col, int sourceRow, int sourceCol, Point2D[][]edgeTo, double[][] distTo){
-        double sourceDist = distTo[sourceRow][sourceCol];
-        if (energies[row][col] + sourceDist < distTo[row][col]) {
-            edgeTo[row][col] = new Point2D(sourceRow, sourceCol);
-            distTo[row][col] = energies[row][col] + sourceDist;
-        }
-    }
-
-    public void removeHorizontalSeam(int[] seam) {
-        transposeH();
-        removeVerticalSeam(seam);
-        orientation = ORIENTATION.HORIZONTAL;
-    }
-
-    public void removeVerticalSeam(int[] seam) {
-        transposeV();
-        validateSeam(seam);
-
-        Color[][] newColors = new Color[height][width - 1];
-        for (int row=0; row < height; row++) {
-            for (int col=0; col < width; col++) {
-                if (col < seam[row])
-                    newColors[row][col] = colors[row][col];
-                else if (col > seam[row])
-                    newColors[row][col - 1] = colors[row][col];
-            }
-        }
-        width--;
-        colors = newColors;
-        fillEnergies();
-    }
-
-    private void validateSeam(int[] seam){
-        if (width <= 1 ) throw new java.lang.IllegalArgumentException("Cannot remove seam");
-
-        if (seam == null || seam.length != height) throw new java.lang.IllegalArgumentException("Invalid seam: wrong length");
-
-        for (int i = 0; i < height - 2; i++) { 
-
-            if (seam[i] < 0 || seam[i] >= width) throw new java.lang.IllegalArgumentException("Invalid seam: seam out of bound");
-
-            boolean adjacentVertices = Math.abs(seam[i+1] - seam[i]) <= 1;
-            if (!adjacentVertices) {
-                throw new java.lang.IllegalArgumentException("Invalid seam: unadjacent vertex");
-            }
-
-        }
-    }
-
-    public double energy(int col,  int row)  {
-        if (col < 0 || col >= width() || row < 0 || row >= height()){
-            throw new java.lang.IllegalArgumentException("Coordinates out of bounds");
-        }
-
-        if (col == 0 || col == width() - 1 || row == 0 || row == height() - 1)
-            return 1000;
-
-        return orientation == ORIENTATION.VERTICAL ? gradient(col, row) : gradient(row, col);
-    }
-
-    private void fillEnergies() {
-        energies = new double[height][width];
-        for (int row=0; row < height; row++){
-            for (int col=0; col < width; col++) {
-                if (orientation == ORIENTATION.VERTICAL)
-                    energies[row][col] = energy(col, row);
+        x=v%width;   //col
+        y=v/width;   //row
+        if(x==0||x==width-1||y==height-1)
+            return;
+        //choose the three pixels(w) below v
+        for(int k=-1;k<2;k++){
+            w=(y+1)*width+x+k;
+            if(isVertical)
+                weight=energy(x+k, y+1);
+            else
+                weight=energy(y+1, x+k);
+            if(distTo[w]>distTo[v]+weight) {
+                distTo[w]=distTo[v]+weight;
+                edgeTo[w]=v;
+                if(y+1==height-1)
+                    return;
+                if(pq.contains(w))
+                    pq.change(w, distTo[w]);
                 else
-                    energies[row][col] = energy(row, col);
+                    pq.insert(w, distTo[w]);
             }
         }
     }
-
-    private void transposeH(){
-        if (orientation == ORIENTATION.VERTICAL) transpose();
-        orientation = ORIENTATION.VERTICAL;
-    }
-
-    private void transposeV() {
-        if (orientation == ORIENTATION.HORIZONTAL) transpose();
-        orientation = ORIENTATION.VERTICAL;
-    }
-
-    private void transpose() {
-        int tmp = height;
-        height = width;
-        width = tmp;
-
-        Color[][] transposed = new Color[height][width];
-        for (int row=0; row < height; row++) {
-            for (int col=0; col < width; col++){
-                transposed[row][col] = colors[col][row];
+    private int[]findSeam(boolean isVertical){
+        int width=width();
+        int height=height();
+        if(!isVertical) {
+            width=height();
+            height=width();
+        }
+        double minDist=Double.POSITIVE_INFINITY;
+        int minIndex=0;
+        int[]seamPath=new int[height];
+        int start=width*height-width;
+        for(int i=0;i<width;i++) {
+            if(distTo[start+i]<minDist) {
+                minDist=distTo[start+i];
+                minIndex=start+i;
             }
         }
-
-        colors = transposed;
-        fillEnergies();
+        //if i==0,edgeTo[minIndex] is not 
+        for(int i=height-1;i>0;i--) {
+            seamPath[i]=minIndex%width;   //i is the row,seamPath[i] is the col
+            minIndex=edgeTo[minIndex];
+        }
+        //the first path is vertical
+        if(seamPath.length>1)
+            seamPath[0]=seamPath[1];
+        return seamPath;
     }
-
+    
+    
+    public int[] findHorizontalSeam() {               // sequence of indices for horizontal seam
+        DijkstraSP(false);
+        return findSeam(false);
+    }
+    public int[] findVerticalSeam() {                 // sequence of indices for vertical seam
+        DijkstraSP(true);
+        return findSeam(true);
+    }
+    
+    public void removeHorizontalSeam(int[] seam) {   // remove horizontal seam from current picture
+        int height=height();
+        int width=width();
+        if(seam==null)
+            throw new java.lang.IllegalArgumentException();
+        if(seam.length!=width||height<=1)
+            throw new java.lang.IllegalArgumentException();
+        Color[][]copy=new Color[height-1][width];
+        for(int x=0;x<width;x++) {
+            if(seam[x]<0||seam[x]>=height)
+                throw new java.lang.IllegalArgumentException();
+            for(int y=0;y<height;y++) {
+                if(y<seam[x])
+                    copy[y][x]=colorMatrix[y][x];
+                else if(y>seam[x])
+                    copy[y-1][x]=colorMatrix[y][x];
+            }
+        }
+        colorMatrix=copy;
+        picture();  
+    }
+    
+    public void removeVerticalSeam(int[] seam) {     // remove vertical seam from current picture
+        int height=height();
+        int width=width();
+        if(seam==null)
+            throw new java.lang.IllegalArgumentException();
+        if(seam.length!=height||width<=1)
+            throw new java.lang.IllegalArgumentException();
+        Color[][]copy=new Color[height][width-1];
+        for(int y=0;y<height;y++) {
+            if(seam[y]<0||seam[y]>=width)
+                throw new java.lang.IllegalArgumentException();
+            for(int x=0;x<width;x++) {
+                if(x<seam[y])
+                    copy[y][x]=colorMatrix[y][x];
+                else if(x>seam[y])
+                    copy[y][x-1]=colorMatrix[y][x]; 
+            }
+        }
+        colorMatrix=copy;
+        picture();
+    }
+    
+    public static void main(String[] args) {
+        
+    }
 }
