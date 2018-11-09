a9fcc0ec2bed1d3b7a8bc8e9e222314683ac8e52
samyumobiMSIT <samyumobi@msitprogram.net>
2018-11-09 09:39:09 +0530

    "ads2_m10_a1_1: 9/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practise/Seam Carving/SeamCarver.java
+++ practise/Seam Carving/SeamCarver.java
@@ -1,237 +1,216 @@
-import java.awt.*;
-import java.awt.geom.Arc2D;
-
+import java.awt.Color;
+import java.util.Arrays;
+import java.lang.Math;
 
 public class SeamCarver {
-    private Picture picture;
-    private int width;
-    private int height;
-    private int[][] rgb;
-    private double[][] energy;
-
-    private double[] seamDist;
-    private int[] seamPath;
-
+    public int BORDER_ENERGY = 1000;
+    public int width;
+    public int height;
+    public Picture picture;
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
+        if(picture == null) {
+            throw new IllegalArgumentException("picture is null");
+        }
+        this.width = picture.width();
+        this.height = picture.height();
         this.picture = picture;
-        width = picture.width();
-        height = picture.height();
-
-        setRGB(picture);
-        initEnergy();
-
     }
-
-    private void initEnergy() {
-        energy = new double[height][width];
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                setEnergy(i, j);
-            }
-        }
-    }
-
-    private void setRGB(Picture picture) {
-        rgb = new int[height][width];
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                rgb[i][j] = picture.get(j, i).getRGB();
-            }
-        }
-    }
-
-    private void setEnergy(int i, int j) {
-        if (i == 0 || j == 0 || i == height - 1 || j == width - 1)
-            energy[i][j] = 1000000;
-        else
-            energy[i][j] = deltaX(j, i) + deltaY(j, i);
-    }
-
-
     // current picture
     public Picture picture() {
-        Picture p = new Picture(width, height);
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                    p.set(j, i, new Color(rgb[i][j]));
-            }
-        }
-        picture = p;
-        setRGB(picture);
-        initEnergy();
         return picture;
     }
     // width of current picture
-    public     int width() {
+    public int width() {
         return width;
     }
+
     // height of current picture
-    public     int height() {
+    public int height() {
         return height;
     }
+
     // energy of pixel at column x and row y
-    public  double energy(int x, int y) {
-        if (x >= width() || y >= height() || x < 0 || y < 0)
-            throw new java.lang.IndexOutOfBoundsException("picture is null");
-        return energy[y][x];
-    }
-    //rgb number of col x, row y
-    private int getRed(int x, int y) {
-        return (rgb[y][x] >> 16) & 0xFF;
+    public double energy(int x, int y) {
+        if (x == 0 || x == width - 1 || y == 0 || y == height - 1) {
+            return BORDER_ENERGY;
+        }
+
+        int deltaX = calculateSquare(x + 1, y, x - 1, y);
+        int deltaY = calculateSquare(x, y + 1, x, y - 1);
+        return Math.sqrt(deltaX + deltaY);
     }
 
-    private int getGreen(int x, int y) {
-        return (rgb[y][x] >> 8) & 0xFF;
+    public int calculateSquare(int x1, int y1, int x2, int y2) {
+        Color color1 = picture.get(x1, y1);
+        Color color2 = picture.get(x2, y2);
+        int red = color1.getRed() - color2.getRed();
+        int green = color1.getGreen() - color2.getGreen();
+        int blue = color1.getBlue() - color2.getBlue();
+        return red * red + green * green + blue * blue;
     }
 
-    private int getBlue(int x, int y) {
-        return (rgb[y][x] >> 0) & 0xFF;
-    }
-    //delta value of col x, row y
-    private double deltaX(int x, int y) {
-        int reddiff = getRed(x + 1, y) - getRed(x - 1, y);
-        int greendiff = getGreen(x + 1, y) - getGreen(x - 1, y);
-        int bluediff = getBlue(x + 1, y) - getBlue(x - 1, y);
-        return reddiff*reddiff + greendiff*greendiff + bluediff*bluediff;
-    }
+    public double[][] transpose(double[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-    private double deltaY(int x, int y) {
-        int reddiff = getRed(x, y + 1) - getRed(x, y - 1);
-        int greendiff = getGreen(x, y + 1) - getGreen(x, y - 1);
-        int bluediff = getBlue(x, y + 1) - getBlue(x, y - 1);
-        return reddiff*reddiff + greendiff*greendiff + bluediff*bluediff;
+        double transpose[][] = new double[n][m];
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                transpose[j][i] = matrix[i][j];
+        return transpose;
     }
 
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
-        findSP(height, width, false);
-        int[] result = new int[width];
-        for (int v = seamPath[width*height + 1]; v > 0; v = seamPath[v])
-            result[(v - 1) / height] = (v - 1) % height;
-        return result;
+        double[][] sum = new double[width][height];
+        int[][] parent = new int[width][height];
+        for (int y = 0; y < height; ++y) {
+            sum[0][y] = BORDER_ENERGY;
+            parent[0][y] = y;
+        }
+
+        for (int x = 1; x < width; ++x) {
+            for (int y = 0; y < height; ++y) {
+                double temp = sum[x - 1][y];
+                // parent[x][y] = y;
+                if (y > 0 && sum[x - 1][y - 1] < temp) {
+                    temp = sum[x - 1][y - 1];
+                    // parent[x][y] = y - 1;
+                }
+
+                if (y < height - 1 && sum[x - 1][y + 1] < temp) {
+                    temp = sum[x - 1][y + 1];
+                    // parent[x][y] = y + 1;
+                }
+                sum[x][y] = energy(x, y) + temp;
+            }
+        }
+
+        for (int x = 1; x < width; ++x) {
+            for (int y = 0; y < height; ++y) {
+                double temp = sum[x - 1][y];
+                parent[x][y] = y;
+                if (y > 0 && sum[x - 1][y - 1] < temp) {
+                    temp = sum[x - 1][y - 1];
+                    parent[x][y] = y - 1;
+                }
+
+                if (y < height - 1 && sum[x - 1][y + 1] < temp) {
+                    temp = sum[x - 1][y + 1];
+                    parent[x][y] = y + 1;
+                }
+            }
+        }
+
+        int index = 0;
+        for (int y = 1; y < height; ++y) {
+            if (sum[width - 1][y] < sum[width - 1][index]) {
+                index = y;
+            }
+        }
+        int[] seam = new int[width];
+        seam[width - 1] = index;
+        for (int x = width - 2; x >= 0; --x) {
+            seam[x] = parent[x + 1][index];
+            index = parent[x + 1][index];
+        }
+        return seam;
     }
+
     // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
-        findSP(width, height, true);
-        int[] result = new int[height];
-        for (int v = seamPath[width*height + 1]; v > 0; v = seamPath[v])
-            result[(v - 1) / width] = (v - 1) % width;
-        return result;
-    }
-
-    private void findSP(int width, int height, boolean vertical) {
-        int n = width*height;
-        seamDist = new double[n + 2];
-        seamPath = new int[n + 2];
-
-        for (int i = 1; i < n + 2; i++) {
-            if (i <= width) seamDist[i] = 0;
-            else seamDist[i] = Double.POSITIVE_INFINITY;
+        double[][] sum = new double[width][height];
+        int[][] parent = new int[width][height];
+        for (int x = 0; x < width; ++x) {
+            sum[x][0] = BORDER_ENERGY;
+            parent[x][0] = x;
         }
-        seamDist[0] = 0;
+        // adding pixel energy with least adjucent pixel
+        for (int y = 1; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                double temp = sum[x][y - 1];
+                // parent[x][y] = x;
+                if (x > 0 && sum[x - 1][y - 1] < temp) {
+                    temp = sum[x - 1][y - 1];
+                    // parent[x][y] = x - 1;
+                }
 
-        for (int i = width + 1; i < n + 1; i++) {
-            relax(i, width, vertical);
+                if (x < width - 1 && sum[x + 1][y - 1] < temp) {
+                    temp = sum[x + 1][y - 1];
+                    // parent[x][y] = x + 1;
+                }
+                sum[x][y] = energy(x, y) + temp;
+            }
         }
+        // finding pixcels with least adjucent pixel
+        for (int y = 1; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                double temp = sum[x][y - 1];
+                parent[x][y] = x;
+                if (x > 0 && sum[x - 1][y - 1] < temp) {
+                    temp = sum[x - 1][y - 1];
+                    parent[x][y] = x - 1;
+                }
 
-        for (int i = n - width + 1; i < n + 1; i++) {
-            relax(n + 1, i, 1000000);
+                if (x < width - 1 && sum[x + 1][y - 1] < temp) {
+                    temp = sum[x + 1][y - 1];
+                    parent[x][y] = x + 1;
+                }
+            }
         }
-    }
-
-    private void relax(int i, int width, boolean vertical) {
-        if (vertical) relaxV(i, width);
-        else relaxH(i, width);
-    }
-
-    private void relaxV(int i, int width) {
-        int x = (i - 1) % width;
-        int y = (i - 1) / width;
-
-        if (x > 0) relax(i, i - width - 1, energy(x - 1, y - 1));
-        if (x < width - 1) relax(i, i - width + 1, energy(x + 1, y - 1));
-        relax(i, i - width, energy(x, y - 1));
-    }
-
-    private void relaxH(int i, int width) {
-        int x = (i - 1) / width;
-        int y = (i - 1) % width;
-
-        if (y % width > 0) relax(i, i - width - 1, energy(x - 1, y - 1));
-        if (y % width < width - 1) relax(i, i - width + 1, energy(x - 1, y + 1));
-        relax(i, i - width, energy(x - 1, y));
-    }
-
-    private void relax(int to, int from, double e) {
-        if (seamDist[to] > seamDist[from] + e) {
-            seamDist[to] = seamDist[from] + e;
-            seamPath[to] = from;
+        int index = 0;
+        for (int x = 1; x < width; ++x) {
+            if (sum[x][height - 1] < sum[index][height - 1]) {
+                index = x;
+            }
         }
+        int[] seam = new int[height];
+        seam[height - 1] = index;
+        for (int y = height - 2; y >= 0; --y) {
+            seam[y] = parent[index][y + 1];
+            index = parent[index][y + 1];
+        }
+        return seam;
     }
 
     // remove horizontal seam from current picture
-    public    void removeHorizontalSeam(int[] seam) {
-        height--;
-        for (int i = 0; i < width; i++) {
-            int rm = seam[i];
-            int j = 0;
-            while (j < height) {
-                if (j >= rm) {
-                    rgb[j][i] = rgb[j + 1][i];
-                    energy[j][i] = energy[j + 1][i];
-                }
-                j++;
+    public void removeHorizontalSeam(int[] seam) {
+
+        if (seam == null || seam.length != width) {
+            throw new IllegalArgumentException("Seam is illegal.");
+        }
+
+        if (height <= 1) {
+            throw new IllegalArgumentException("Height of the picture is less than or equal to 1");
+        }
+
+        for (int x = 0; x < width; ++x) {
+            for (int y = seam[x]; y < height - 1; ++y) {
+                picture.set(x, y, picture.get(x, y + 1));
             }
         }
 
-        for (int i = 0; i < width; i++) {
-            int rm = seam[i];
-            setEnergy(rm, i);
-            if (i > 0) setEnergy(rm, i - 1);
-            if (i < width - 1) setEnergy(rm, i + 1);
-            if (rm > 0) setEnergy(rm - 1, i);
-            if (rm < height - 1) setEnergy(rm + 1, i);
-        }
+        --height;
     }
-
-
 
     // remove vertical seam from current picture
-    public    void removeVerticalSeam(int[] seam) {
-        width--;
-        for (int i = 0; i < height; i++) {
-            int rm = seam[i];
-            int j = 0;
-            while (j < width) {
-                if (j >= rm) {
-                    rgb[i][j] = rgb[i][j + 1];
-                    energy[i][j] = energy[i][j + 1];
-                }
+    public void removeVerticalSeam(int[] seam) {
 
-                j++;
+        if (seam == null || seam.length != height) {
+            throw new IllegalArgumentException("Seam is illegal.");
+        }
+
+        if (width <= 1) {
+            throw new IllegalArgumentException("Width of the picture is less than or equal to 1");
+        }
+
+        for (int y = 0; y < height; ++y) {
+            for (int x = seam[y]; x < width - 1; ++x) {
+                picture.set(x, y, picture.get(x + 1, y));
             }
         }
 
-        for (int i = 0; i < height; i++) {
-            int rm = seam[i];
-            setEnergy(i, rm);
-            if (i > 0) setEnergy(i - 1, rm);
-            if (i < width - 1) setEnergy(i + 1, rm);
-            if (rm > 0) setEnergy(i, rm - 1);
-            if (rm < height - 1) setEnergy(i, rm + 1);
-        }
-    }
-
-    public static void printEnergy(SeamCarver sc) {
-        System.out.printf("Printing energy calculated for each pixel.\n");
-
-        for (int j = 0; j < sc.height(); j++)
-        {
-            for (int i = 0; i < sc.width(); i++)
-                System.out.printf("%9.0f ", sc.energy(i, j));
-
-            System.out.println();
-        }
+        --width;
     }
 }
