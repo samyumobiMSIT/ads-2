20b6d1c608ef592409d5cbe687dc706907663482
samyumobiMSIT <samyumobi@msitprogram.net>
2018-11-14 14:26:03 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m14/Assignment-1/Solution.java
+++ assignments/m14/Assignment-1/Solution.java
@@ -1,24 +1,41 @@
 import java.util.Scanner;
- public class Solution {
-    public static void main(String[] args) {
-         Scanner scan = new Scanner(System.in);
-         TST<Integer> st = new TST<Integer>();
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * main method that drives the program.
+     * @param      args  The arguments
+     * Time complexity for this method is O(words * w.length())
+     */
+    public static void main(final String[] args) {
+        TST<Integer> t = new TST();
         String[] words = loadWords();
         //Your code goes here...
-        for(int i = 0;i< words.length;i++){
-            st.put(words[i], i);
+        Scanner sc = new Scanner(System.in);
+        int flag = 0;
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words[i].length(); j++) {
+                t.put(words[i].substring(j), flag);
+                flag++;
+            }
         }
-        String req = scan.nextLine();
-        //int sum = 0;
-        for (String s : st.keysWithPrefix(req)) {
-            System.out.println(s);
-            //sum +=1;
+        for (String res : t.keysWithPrefix(sc.nextLine())) {
+            System.out.println(res);
         }
-        //System.out.println(sum);
     }
+    /**
+     * Loads words.
+     * @return     words of type String array.
+     */
     public static String[] loadWords() {
         In in = new In("/Files/dictionary-algs4.txt");
         String[] words = in.readAllStrings();
         return words;
     }
-} 
+}
