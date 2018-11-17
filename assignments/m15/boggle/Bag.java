9d83078c07d80d51a2dd708e5383b3a1ba32e8ba
samyumobiMSIT <samyumobi@msitprogram.net>
2018-11-17 11:38:11 +0530

    "ads2_m16_a1_1: 14/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- assignments/m15/boggle/Bag.java
+++ assignments/m15/boggle/Bag.java
@@ -1,89 +1,122 @@
-/*************************************************************************
- *  Compilation:  javac Bag.java
- *  Execution:    java Bag < input.txt
- *
- *  A generic bag or multiset, implemented using a linked list.
- *
- *************************************************************************/
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
 /**
- *  The <tt>Bag</tt> class represents a bag (or multiset) of 
- *  generic items. It supports insertion and iterating over the 
- *  items in arbitrary order.
- *  <p>
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation 
- *  take constant time. Iteration takes time proportional to the number of items.
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * Class for bag.
+ *
+ * @param      <Item>  The item
  */
 public class Bag<Item> implements Iterable<Item> {
-    private int N;         // number of elements in bag
-    private Node first;    // beginning of bag
-
-    // helper linked list class
+    /**
+     * number of elements in bag .
+     */
+    private int n;
+    /**
+     * pointer at beginning of bag .
+     */
+    private Node first;
+    /**
+     * Class for node.
+     *
+     */
     private class Node {
+        /**
+         *  variable for item .
+         */
         private Item item;
+        /**
+         *  variable for next node .
+         */
         private Node next;
     }
-
-   /**
-     * Create an empty stack.
+    /**
+     * Constructs the object.
+     *
      */
     public Bag() {
         first = null;
-        N = 0;
+        n = 0;
     }
 
-   /**
-     * Is the BAG empty?
+    /**
+     * Determines if empty.
+     * its complexity is O(1).
+     *
+     *
+     * @return     True if empty, False otherwise.
      */
     public boolean isEmpty() {
         return first == null;
     }
 
-   /**
-     * Return the number of items in the bag.
+    /**
+     *  number of items in the bag .
+     *  its complexity is O(1).
+     *
+     * @return     { size}
      */
     public int size() {
-        return N;
+        return n;
     }
 
-   /**
-     * Add the item to the bag.
+    /**
+     * Add the item to the bag .
+     * its complexity is O(1).
+     *
+     * @param      item  The item
      */
-    public void add(Item item) {
+    public void add(final Item item) {
         Node oldfirst = first;
         first = new Node();
         first.item = item;
         first.next = oldfirst;
-        N++;
+        n++;
     }
 
 
-   /**
-     * Return an iterator that iterates over the items in the bag.
+    /**
+     * { Return an iterator that iterates over the items in the bag }.
+     * its complexity is O(N).
+     *
+     * @return     { item }
      */
     public Iterator<Item> iterator()  {
-        return new ListIterator();  
+        return new ListIterator();
     }
 
-    // an iterator, doesn't implement remove() since it's optional
+    /**
+     * Class for list iterator.
+     */
     private class ListIterator implements Iterator<Item> {
+        /**
+         * { current node }.
+         */
         private Node current = first;
-
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
+        /**
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         */
+        public boolean hasNext() {
+            return current != null;
+        }
+        /**
+         * { function for removeing }.
+         */
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+        /**
+         * { function for next }.
+         *
+         * @return     { item }
+         */
         public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
             Item item = current.item;
-            current = current.next; 
+            current = current.next;
             return item;
         }
     }
-
 }
--- assignments/m15/boggle/BoggleSolver.java
+++ assignments/m15/boggle/BoggleSolver.java
@@ -1,141 +1,140 @@
-import java.util.Set;
-import java.util.TreeSet;
-
+import java.util.HashSet;
+/**.
+ * Class for boggle solver.
+ */
 public class BoggleSolver {
-   
-   private static final char Q_LETTER = 'Q';
-   
-   private static final String QU_STRING = "QU";
-   
-   private final TST<Integer> dictionary;
-   
-   // Initializes the data structure using the given array of strings as the dictionary.
-   // (each word in the dictionary contains only the uppercase letters A through Z.)
-   public BoggleSolver(final String[] dictionary) {
-      // shuffle alphabet dictionary to create more balanced trie
-      shuffle(dictionary);
-      this.dictionary = new TST<Integer>();
-      int[] points = {0, 0, 0, 1, 1, 2, 3, 5, 11};
-      for (String s : dictionary) {
-         // if maxpoint word
-         if (s.length() >= points.length - 1) {
-            this.dictionary.put(s, points[points.length - 1]);
-         }
-         else {
-            this.dictionary.put(s, points[s.length()]);
-         }
-      }
-   }
+    /**.
+     * { var_description }
+     */
+    private TrieSET trie;
+    /**.
+     * { var_description }
+     */
+    public static final int T_H_R_E_E = 3;
+    /**.
+     * { var_description }
+     */
+    public static final int F_O_U_R = 4;
+    /**.
+     * { var_description }
+     */
+    public static final int F_I_V_E = 5;
+    /**.
+     * { var_description }
+     */
+    public static final int S_I_X = 6;
+    /**.
+     * { var_description }
+     */
+    public static final int S_E_V_E_N = 7;
+    /**.
+     * { var_description }
+     */
+    public static final int E_L_E = 11;
+    /**.
+     * Constructs the object.
+     *
+     * @param      dictionary  The dictionary
+     */
+    public BoggleSolver(final String[] dictionary) {
+        trie = new TrieSET();
+        for (int i = 0; i < dictionary.length; i++) {
+            trie.add(dictionary[i]);
+        }
+    }
+    /**.
+     * Gets all valid words.
+     *
+     * @param      board  The board
+     *
+     * @return     All valid words.
+     */
+    public HashSet<String> getAllValidWords(final BoggleBoard board) {
+        HashSet<String> words = new HashSet<String>();
 
-   // Knuth shuffling algorithm
-   private void shuffle(String[] dictionary) {
-      for (int i = 1; i < dictionary.length; i++) {
-         // choose random string from left part of i including i
-         int randLeftIndex = (int) (Math.random() * (i + 1) );
-         String buffer = dictionary[randLeftIndex];
-         dictionary[randLeftIndex] = dictionary[i];
-         dictionary[i] = buffer;
-      }
-   }
+        for (int i = 0; i < board.rows(); i++) {
+            for (int j = 0; j < board.cols(); j++) {
+                boolean[][] visited = new boolean[
+                board.rows()][board.cols()];
+                searchWord(board, i, j, "", visited, words);
+            }
 
-   // Returns the set of all valid words in the given Boggle board, as an Iterable.
-   public Iterable<String> getAllValidWords(BoggleBoard board) {
-      if (board == null) 
-         throw new java.lang.IllegalArgumentException("board is null");
-      // choose set because each word must appears only once
-      Set<String> foundWords = new TreeSet<String>();
-      // for all dices as first letter
-      for (int row = 0; row < board.rows(); row++) {
-         for (int col = 0; col < board.cols(); col++) {
-            // current watching string
-            String charSequence = addLetter("", board.getLetter(row, col));
-            // array with visited dices
-            boolean marked[][] = new boolean[board.rows()][board.cols()];
-            marked[row][col] = true;
-            dfs(foundWords, charSequence, marked, row, col, board);
-         }
-      }
-      return foundWords;
-   }
-
-   // depth first search for searching valid words
-   private void dfs(Set<String> foundWords, String charSequence, boolean[][] marked,
-         int startRow, int startCol, BoggleBoard board) {
-      // add valid word to set
-      if (isValidWord(charSequence) ) foundWords.add(charSequence);
-      // check all adjacent dices 
-      // max & min methods to avoid going beyond the borders of board
-      // start upper left adjacent dice
-      for (int row = Math.max(0, startRow - 1); row <= Math.min(board.rows() - 1, startRow + 1); row++) {
-         for (int col = Math.max(0, startCol - 1); col <= Math.min(board.cols() - 1,startCol + 1); col++) {
-            if (marked[row][col]) continue;
-            if (!dictionary.hasPrefix(charSequence)) continue;
-            // prepare to recursive call
-            marked[row][col] = true;
-            dfs(foundWords, addLetter(charSequence, board.getLetter(row, col)), marked, row, col, board);
-            // roll back after recursive call
-            marked[row][col] = false;
-         }
-      }
-   }
-
-   private String addLetter(String to, char letter) {
-      if (letter == Q_LETTER) return to + QU_STRING;
-      else return to + letter;
-   }
-   
-   private boolean isValidWord(String currentWord) {
-      if (currentWord == null) return false;
-      if (dictionary.contains(currentWord) && currentWord.length() > 2) return true;
-      else return false;
-   }
-   
-   // Returns the score of the given word if it is in the dictionary, zero otherwise.
-   // (the word contains only the uppercase letters A through Z.)
-   public int scoreOf(String word) {
-      if (word == null || word.length() == 0) 
-         throw new java.lang.IllegalArgumentException("You want to score empty string!");
-      Integer score = dictionary.get(word);
-      if (score == null) 
-         return 0;
-      else 
-         return score;
-   }
-   
-   // Test
-   public static void main(String[] args) {
-
-    In in = new In(args[0]);
-      String[] dictionary = in.readAllStrings();
-      BoggleSolver solver = new BoggleSolver(dictionary);
-      BoggleBoard board = new BoggleBoard(args[1]);
-      int score = 0;
-      for (String word : solver.getAllValidWords(board))
-      {
-        StdOut.println(word);
-          score += solver.scoreOf(word);
-      }
-      StdOut.println("Score = " + score);
-  }
-    
+        }
+        return words;
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      board    The board
+     * @param      i        { parameter_description }
+     * @param      j        { parameter_description }
+     * @param      str      The string
+     * @param      visited  The visited
+     * @param      words    The words
+     */
+    private void searchWord(final BoggleBoard board, final int i,
+        final int j, final String str, final boolean[][] visited,
+                            final HashSet words) {
+        if (visited[i][j]) {
+            return;
+        }
+        char ch = board.getLetter(i, j);
+        String word = str;
+        if (ch == 'Q') {
+            word += "QU";
+        } else {
+            word += ch;
+        }
+        if (!trie.hasPrefix(word)) {
+            return;
+        }
+        if (word.length() > 2 && trie.contains(word)) {
+            words.add(word);
+        }
+        visited[i][j] = true;
+        for (int k = -1; k <= 1; k++) {
+            for (int l = -1; l <= 1; l++) {
+                // if (k == 0 && l == 0) {
+                //  continue;
+                // }
+                if ((i + k >= 0) && (i + k < board.rows())
+                        && (j + l >= 0) && (j + l < board.cols())) {
+                    searchWord(board, i + k, j + l, word,
+                               visited, words);
+                }
+            }
+        }
+        visited[i][j] = false;
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      word  The word
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int scoreOf(final String word) {
+        if (trie.contains(word)) {
+            switch (word.length()) {
+            //case 0:
+            //case 1:
+            case 2:
+                return 0;
+            case T_H_R_E_E:
+            case F_O_U_R:
+                return 1;
+            case F_I_V_E:
+                return 2;
+            case S_I_X:
+                return T_H_R_E_E;
+            case S_E_V_E_N:
+                return F_I_V_E;
+            default:
+                return E_L_E;
+            }
+        } else {
+            return 0;
+        }
+    }
 }
-       
-      /* Stress Unit Test
-       String dir = "D:\\ads-2\\assignments\\m15\\boggle\\Files";
-        String dictDir = dir + "dictionary-yawl.txt";
-        String boardDir = dir + "board-points26539.txt";
-       In in = new In(dictDir);
-       String[] dictionary = in.readAllStrings();
-       BoggleSolver solver = new BoggleSolver(dictionary);
-       BoggleBoard board = new BoggleBoard(boardDir);
-       // Stress test
-       // BoggleBoard board = new BoggleBoard(10, 11);
-       int score = 0;
-       for (String word : solver.getAllValidWords(board))
-       {
-           StdOut.println(word + " " + solver.dictionary.get(word));
-           score += solver.scoreOf(word);
-       }
-       StdOut.println("Score = " + score);
-   }
-}*/
+
