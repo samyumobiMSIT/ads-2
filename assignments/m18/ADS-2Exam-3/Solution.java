e398d15e563ff9defab0c4d4f5200ae202144945
samyumobiMSIT <samyumobi@msitprogram.net>
2018-11-17 11:33:23 +0530

    "ads2_m18_a1_1: 4/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m18/ADS-2Exam-3/Solution.java
+++ assignments/m18/ADS-2Exam-3/Solution.java
@@ -1,5 +1,6 @@
-
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Solution {
 
@@ -84,74 +85,98 @@ public class Solution {
 		In in = new In(file);
 		return in.readAllStrings();
 	}
-	/**
-	 * Loads a dictionary.
-	 *
-	 * @param      file  The file
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public static BinarySearchST<String, Integer> loadDictionary(
-		final String file) {
-		BinarySearchST<String, Integer>  st = new
-		BinarySearchST<String, Integer>();
+
+	public static BinarySearchST<String, Integer> loadDictionary(String file) {
+		BinarySearchST<String, Integer>  st = new BinarySearchST<String, Integer>();
 		// your code goes here
-		String[] dict = toReadFile(file);
-		for (int i = 0; i < dict.length; i++) {
-			String str = dict[i].toLowerCase();
-			if (st.contains(str)) {
-				st.put(str , st.get(str) + 1);
+		String[] dictionary = toReadFile(file);
+		for (int i = 0; i < dictionary.length; i++) {
+			String word = dictionary[i].toLowerCase();
+			if (st.contains(word )) {
+				st.put(word , st.get(word ) + 1);
 			} else {
-				st.put(str, 1);
+				st.put(word, 1);
 			}
 		}
 		return st;
 	}
 
 }
-/**
- * Class for t 9.
- */
+
 class T9 {
-	/**
-	 * tst for loading words with freq.
-	 */
-	private TST<Integer> tst;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      st    { parameter_description }
-	 */
+	TST<Integer> tst;
 	public T9(BinarySearchST<String, Integer> st) {
 		// your code goes here
 		tst = new TST<>();
-		for (String eachWord : st.keys()) {
-			tst.put(eachWord, st.get(eachWord));
+		for (String each : st.keys()) {
+			tst.put(each, st.get(each));
 		}
 	}
 
-	//get all the prefixes that match with given prefix.
-	/**
-	 * Gets all words.
-	 *
-	 * @param      prefix  The prefix
-	 *
-	 * @return     All words.
-	 */
-	public Iterable<String> getAllWords(final String prefix) {
+	// get all the prefixes that match with given prefix.
+	public Iterable<String> getAllWords(String prefix) {
 		// your code goes here
 		return tst.keysWithPrefix(prefix);
 	}
 
-	public Iterable<String> potentialWords(final String t9Signature) {
+	public Iterable<String> potentialWords(String t9Signature) {
 		// your code goes here
-		return null;
+		// String[] nums = t9Signature.split("");
+		ArrayList<String> list = new ArrayList<>();
+		for(String each: tst.keys()) {
+			String[] word = each.split("");
+			String num = "";
+			for(String ch : word){
+				if(ch.equals("a") || ch.equals("b") || ch.equals("c")) {
+					num = num + "2";
+				}
+				if(ch.equals("d") || ch.equals("e") || ch.equals("f")) {
+					num = num + "3";
+				}
+				if(ch.equals("g") || ch.equals("h") || ch.equals("i")) {
+					num = num + "4";
+				}
+				if(ch.equals("j") || ch.equals("k") || ch.equals("l")) {
+					num = num + "5";
+				}
+				if(ch.equals("m") || ch.equals("n") || ch.equals("o")) {
+					num = num + "6";
+				}
+				if(ch.equals("p") || ch.equals("q") || ch.equals("r") || ch.equals("s")) {
+					num = num + "7";
+				}
+				if(ch.equals("t") || ch.equals("u") || ch.equals("v")) {
+					num = num + "8";
+				}
+				if(ch.equals("w") || ch.equals("x") || ch.equals("y") || ch.equals("z")) {
+					num = num + "9";
+				}
+			}
+			if(num.equals(t9Signature)) {
+				list.add(each);
+			}
+		} 
+		return list;
 	}
 
 	// return all possibilities(words), find top k with highest frequency.
 	public Iterable<String> getSuggestions(Iterable<String> words, int k) {
 		// your code goes here
-		return null;
+		ArrayList<String> al = new ArrayList<>();
+		MaxPQ<Integer> maxpq = new MaxPQ<>();
+		for(String each : words) {
+			maxpq.insert(tst.get(each));
+		}
+		for(int i =0;i<k;i++) {
+			int f = maxpq.delMax();
+			for(String word : words) {
+				if(f == tst.get(word)) {
+					al.add(word);
+				}
+			}
+		}
+		Collections.sort(al);
+		return al;
 	}
 
 	// final output
